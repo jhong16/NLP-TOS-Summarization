320ea6e9e9a1716137c59adefd08b164d52a9e71
Mitchell Falkow <mdfalkow@gmail.com>
2019-04-06 20:10:26 -0400

    BLEU wrapper class done (untested)

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,6 +1,6 @@
 from types import List
 
-from nltk import translate
+from nltk.translate import bleu_score
 
 class Metric:
   @staticmethod
@@ -14,11 +14,15 @@ class Metric:
 class BLEU(Metric):
   @staticmethod
   def sentence_score(expected: str, actual: str) -> float:
-    raise NotImplementedError
+    ref = [expected.split()]
+    hyp = actual.split()
+    return bleu_score.sentence_bleu(ref, hyp)
   
   @staticmethod
-  def corpus_score(expected_corpus: List, actual_corpus: List) -> float:
-    raise NotImplementedError
+  def corpus_score(expected_corpus: List[str], actual_corpus: List[str]) -> float:
+    refs = [[e.split()] for e in expected_corpus]
+    hyps = [a.split() for a in actual_corpus]
+    return bleu_score.corpus_bleu(refs, hyps)
 
 class ROUGE(Metric):
   @staticmethod
