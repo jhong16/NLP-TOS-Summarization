dbf32a8c46be14bd26f2de97a6b68f6e0f616bc8
Mitchell Falkow <mdfalkow@gmail.com>
2019-04-25 18:41:39 -0400

    moved corpus, made json file (stores metadata about extracted tos text)

--- parsing/json_to_plaintext.py
+++ parsing/json_to_plaintext.py
@@ -2,20 +2,29 @@ import os
 import json
 from argparse import ArgumentParser
 
-def output_path(output_dir, file_id):
-    return '{}/{:06d}.txt'.format(output_dir, file_id)
+def output_path(output_dir, filename):
+    return '{}/{}'.format(output_dir, filename)
 
 def main(input_file, output_dir):
     with open(input_file, 'r') as fp:
         input_data = json.load(fp)
     
+    meta = {}
     file_id = 0
     for company in input_data:
         for doc in input_data[company]:
-            with open(output_path(output_dir, file_id), "w") as fp:
-                fp.write(input_data[company][doc])
-            file_id += 1
+            output_file = "{:06d}.txt".format(file_id)
 
+            meta[output_file] = "{}__{}".format(company, doc)
+
+            with open(output_path(output_dir, output_file), "w") as fp:
+                fp.write(input_data[company][doc])
+
+            file_id += 1
+    
+    # generate key file
+    with open("{}/files.json".format(output_dir), "w") as fp:
+        json.dump(meta, fp)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
